46be8dc756c3267e83c3810385221a851629f6d8
yararagi <103433630+yararagi@users.noreply.github.com>
2024-04-04 12:56:53 +0200

    Update MenuView.java

--- src/menu/view/MenuView.java
+++ src/menu/view/MenuView.java
@@ -2,11 +2,8 @@ package menu.view;
 
 import static main.Main.raylib;
 
-import com.raylib.java.core.rCore;
 import com.raylib.java.raymath.Vector2;
 import com.raylib.java.text.Font;
-import com.raylib.java.text.rText;
-import com.raylib.java.textures.Image;
 import com.raylib.java.textures.Texture2D;
 import com.raylib.java.textures.rTextures;
 
@@ -18,11 +15,9 @@ public class MenuView {
     private byte nScritte;
     private String[] scritte;
     private byte offsetScritte;
-    private int screenX;
     private byte fontSize;
     private Font fontRegular;
     private Texture2D backGround;
-    private Image backgroundIMG;
     //background image
     // boh altro
 
@@ -33,35 +28,31 @@ public class MenuView {
         scritte[0]=Stato.GIOCA.toString().toLowerCase();
         scritte[1]=Stato.RISULTATI.toString().toLowerCase();
         scritte[2]=Stato.ESCI.toString().toLowerCase();
-        fontSize=35;
-        screenX=rCore.GetScreenWidth();
+        fontSize=45;
         fontRegular= raylib.text.LoadFont("font\\MagicBreadRegular.ttf");
-        backgroundIMG=rTextures.LoadImage("texture\\background\\city-town-pixel-artwork-aesthetic.jpg");
-        raylib.textures.ImageResizeNN(backgroundIMG, screenX, rCore.GetScreenHeight());
-        backGround= rTextures.LoadTextureFromImage(backgroundIMG);
-        rTextures.UnloadImage(backgroundIMG);
+        backGround= rTextures.LoadTexture("texture\\background\\city-town-pixel-artwork-aesthetic.jpg");
     }
 
     public byte getnScritte() {
         return nScritte;
     }
 
-    public void paintScritte(byte scelta){
-        raylib.core.BeginDrawing();
-        raylib.core.ClearBackground(Color.BLACK);
+    private void paintScritte(byte scelta){
         raylib.textures.DrawTexture(backGround, 0, 0, Color.BLANK);
         for(byte i=0;i<3;i++){
-            raylib.text.DrawTextEx(fontRegular, scritte[i], new Vector2(((screenX/2) - (int)(rText.MeasureTextEx(fontRegular, scritte[i], fontSize, 0).getX()/2)), 175+offsetScritte*i), fontSize, 0, (i==scelta) ? (new Color(164, 22, 26, 150)) : (Color.RAYWHITE));
+            raylib.text.DrawTextEx(fontRegular, scritte[i], new Vector2(150, 200+offsetScritte*i), fontSize, 0, (i==scelta) ? (Color.WHITE) : (new Color(164, 22, 26, 150)));
         }
-        raylib.core.EndDrawing();
     }
 
-    public void paintBackground (){
+    private void paintBackground (){
+        raylib.textures.DrawTextureEx(backGround,new Vector2(0, 0), 0, 1.1f, Color.WHITE);
+    }
+
+    public void paintMenu(byte scelta){
         raylib.core.BeginDrawing();
         raylib.core.ClearBackground(Color.BLACK);
-
-        raylib.textures.DrawTexture(backGround, 0, 0, Color.WHITE);
-
+        this.paintBackground();
+        this.paintScritte(scelta);
         raylib.core.EndDrawing();
     }
 
