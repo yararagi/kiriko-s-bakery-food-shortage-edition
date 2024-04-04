39ec6bd7b877bcd76bddad7593ae9e3ef583a7b0
yararagi <103433630+yararagi@users.noreply.github.com>
2024-04-04 12:56:04 +0200

    fullscreen

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
  
-import java.util.concurrent.Semaphore;
 import com.raylib.java.Raylib;
 
 import gioco.model.Stato;
@@ -13,7 +12,8 @@ public class Main {
     public static Raylib raylib= new Raylib(1920,1080,"kiriko's bakery: food shortage");
    
     public static void main(String[] args) {
-        //Semaphore gestore = new Semaphore(1); //DA IMPLEMENTARE UNA VOLTA SVILUPPATE E TESTATE LE ALTRE SEZIONI DELL'APP
+        raylib.core.MaximizeWindow();
+        raylib.core.SetWindowState(0x00000002);
         MenuController menuController= new MenuController(new MenuView(), new RisultatiView());
         while (statoApp!=Stato.ESCI) {
             //--------------------TEST---------------------
