b978844dc1101e5503464f3899c86e5bd10abe52
yararagi <103433630+yararagi@users.noreply.github.com>
2024-04-04 12:57:09 +0200

    Update RisultatiView.java

--- src/menu/view/RisultatiView.java
+++ src/menu/view/RisultatiView.java
@@ -20,28 +20,30 @@ public class RisultatiView {
 
     public RisultatiView(){
         fontRegular = raylib.text.LoadFont("font\\MagicBreadRegular.ttf");
-        fontSize = 30;
+        fontSize = 40;
         screenX=rCore.GetScreenWidth();
-        offsetScritte=75;
+        offsetScritte=100;
     }
 
-    public void paintRisultati(List<Giocatore> lista) {
+    private void paintRisultati(List<Giocatore> lista) {
         if(lista!=null && lista.size()!=0){
             for(byte i=0;i<lista.size();i++){
-                raylib.text.DrawTextEx(fontRegular, lista.get(i).toString(), new Vector2(((rCore.GetScreenWidth()/2) - (rText.MeasureTextEx(fontRegular, lista.get(i).toString(), fontSize,0).getX()/2 )), 75+offsetScritte*i), fontSize, 0, Color.RAYWHITE);
+                raylib.text.DrawTextEx(fontRegular, lista.get(i).toString(), new Vector2(((rCore.GetScreenWidth()/2) - (rText.MeasureTextEx(fontRegular, lista.get(i).toString(), fontSize,0).getX()/2 )), 125+offsetScritte*i), fontSize, 0, Color.RAYWHITE);
             }
         }else{
-            raylib.text.DrawTextEx(fontRegular, "nessun punteggio", new Vector2(((rCore.GetScreenWidth()/2) - (rText.MeasureTextEx(fontRegular,"nessun punteggio", fontSize,0).getX()/2)), rCore.GetScreenHeight()/2 ), 45, 0, Color.RAYWHITE);
+            raylib.text.DrawTextEx(fontRegular, "nessun punteggio", new Vector2(((rCore.GetScreenWidth()/2) - (rText.MeasureTextEx(fontRegular,"nessun punteggio", 45,0).getX()/2)), rCore.GetScreenHeight()/2 ), 45, 0, Color.RAYWHITE);
         }
     }
 
-    public void paintBack() {
-        raylib.core.BeginDrawing();
-
-        raylib.core.ClearBackground(Color.BLACK);
-        raylib.text.DrawTextEx(fontRegular, "indietro", new Vector2((int)((screenX/2) - (int)(raylib.text.MeasureText("indietro", fontSize)/2)), rCore.GetScreenHeight()-75), fontSize, 0,(new Color(164, 22, 26, 150)));
-        
-        raylib.core.EndDrawing();
+    private void paintBack() {
+        raylib.text.DrawTextEx(fontRegular, "indietro", new Vector2((int)((screenX/2) - (int)(raylib.text.MeasureText("indietro", fontSize+10)/2)), rCore.GetScreenHeight()-150), fontSize+10, 0, new Color(164, 22, 26, 150));
     }
     
+    public void paintSchermataRisultati(List<Giocatore> lista){
+        raylib.core.BeginDrawing();
+        raylib.core.ClearBackground(Color.BLACK);
+        this.paintRisultati(lista);
+        this.paintBack();
+        raylib.core.EndDrawing();
+    }
 }
