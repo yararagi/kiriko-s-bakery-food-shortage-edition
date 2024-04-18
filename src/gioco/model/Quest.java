b64740a97fa7e57a2c3dbacff7cceb36764d51e8
yararagi <montanaroyari@gmail.com>
2024-04-18 08:33:16 +0100

    Quest

--- src/gioco/model/Quest.java
+++ src/gioco/model/Quest.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Quest {
-    Map<Pane, Integer> paniRichiesti= new HashMap<>();
+    Map<TipoPane, Integer> paniRichiesti= new HashMap<>();
 
     public Quest(short nLivello){
-        
+        float moltiplicatore=nLivello*0.1f+((float)(Math.random()*1.5f) +1) ;
+        paniRichiesti.put(TipoPane.BAGUETTE, (int)(10*moltiplicatore));
+        paniRichiesti.put(TipoPane.BRIOCHE, (int)(5*moltiplicatore));
+        paniRichiesti.put(TipoPane.DONUT, (int)(0.6*moltiplicatore));
     }
 }
