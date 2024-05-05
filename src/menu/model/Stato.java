11448da93f870c2a44ae564644658ee45f5723e6
Aron Capraro <capraron17@gmail.com>
2024-05-05 22:28:56 +0200

    thhah

--- src/menu/model/Stato.java
+++ src/menu/model/Stato.java
@@ -1,5 +1,11 @@
 package menu.model;
-
+/**
+ *  l'oggetto Stato viene usato per conservare le varie modalite che il programma può assumere, tali sono:
+ *  MENU: dove l'utente può decidere dove andare
+ *  GIOCA: dove l'utente può giocare al gioco kiriko's bakery food shortage edition
+ *  RISULTATI: dove l'utente può vedere i risultati dei precedenti gioci
+ *  ESCI: dove l'utente può decidere di chiudere l'aplicazione
+ */
 public enum Stato {
     MENU,
     GIOCA,
